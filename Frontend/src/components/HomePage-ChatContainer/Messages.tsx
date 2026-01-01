44a6d35766a7e0a84016db41de12e9cf940f8393
Ziyad Derfoufi <zderfouf@e2r2p11.1337.ma>
2026-01-01 20:43:58 +0100

    [] Chat-container - messages

--- Frontend/src/components/HomePage-ChatContainer/Messages.tsx
+++ Frontend/src/components/HomePage-ChatContainer/Messages.tsx
@@ -1,6 +1,18 @@
+import { useChatStore } from "@/store/useChatStore";
+
 export default function Messages() {
 
-  
+  const messages = useChatStore((state) => state.messages)
+  const getMessages = useChatStore((state) => state.getMessages)
+  const selectedUser = useChatStore((state) => state.selectedUser)
+
+  useEffect(() => {
+    getMessages();
+  }, [getMessages, selectedUser]) 
+
+  messages.map((element) => {
+    if (element._id === )
+  })
   
   return (
     <div className="">
